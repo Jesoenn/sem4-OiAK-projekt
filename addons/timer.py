8b50602311b9d1f365665edfda8bb634429095f5
Jesoenn <147878425+Jesoenn@users.noreply.github.com>
2025-04-30 09:41:40 +0200

    Add files via upload

---
+++ addons/timer.py
@@ -0,0 +1,18 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.time = 0.00
+        self.start_time = 0
+
+    def start(self):
+        """Start measuring time and reset timer"""
+        self.start_time = time.perf_counter()
+        self.time = 0.00
+
+    def stop(self):
+        """Stop timer, [ms], 2 precision"""
+        if self.start_time != 0:
+            self.time = (time.perf_counter() - self.start_time) * 1000  # konwersja na ms
+            self.time = round(self.time, 2)
+            self.start_time = 0
