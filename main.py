3e87413cbb876bfb72840ca485c70a102e7a369b
jbiernacki <jedrx.aegirsson@gmail.com>
2025-05-06 02:59:15 +0200

    Ulepszenie CIOS, testy na czas wykonania

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from addons.word_splitter import *
 from addons.bit_operations import *
 from algorithms.sos import *
 from addons.gcd import *
+from algorithms.cios import *
+import time
 
 # a * b mod n = 14 * 30 mod 11 = 2
 # testowane n: 61,11
 
 s = 3  # words
-w = 2  # word length
-a = 14 # 1110
-b = 30
-n = 11
+w = 100  # word length
+a = 999999999999999999999999999999 # 1110
+b = 999999999999999999999999999999
+n = 97
 n_int=n
 
 r = 1 << (s*w)
@@ -30,9 +32,28 @@ mont_a = int_to_bit_words(mont_a,s,w)
 mont_b = int_to_bit_words(mont_b,s,w)
 
 
-result_mont = monpro_sos(mont_a,mont_b,n,n_prime,w)                # in montgomery space
-final_result=monpro_sos(result_mont, [1,0,0], n, n_prime, w)    # multiply by 1, to get normal number
+start = time.perf_counter()
+result_mont_sos = monpro_sos(mont_a,mont_b,n,n_prime,w)                # in montgomery space
+final_result_sos =monpro_sos(result_mont_sos, [1,0,0], n, n_prime, w)    # multiply by 1, to get normal number
+end = time.perf_counter()
+print("Czas sos: ",(end - start)*1000, "ms")  # milisekundy
 
-print("Oczekiwany wynik: ",(a*b)%n_int)
-print("Wynik: ",bit_words_to_int(final_result,w))
 
+
+start = time.perf_counter()
+result_mont = monpro_cios(mont_a,mont_b,n,n_prime,w)                # in montgomery space
+final_result=monpro_cios(result_mont, [1,0,0], n, n_prime, w)    # multiply by 1, to get normal number
+end = time.perf_counter()
+print("Czas cios: ",(end - start)*1000, "ms")  # milisekundy
+
+
+
+
+print("Oczekiwany wynik cios: ",(a*b)%n_int)
+print("Wynik cios: ",bit_words_to_int(final_result,w))
+
+print()
+
+
+print("Oczekiwany wynik sos: ",(a*b)%n_int)
+print("Wynik sos: ",bit_words_to_int(final_result_sos,w))
